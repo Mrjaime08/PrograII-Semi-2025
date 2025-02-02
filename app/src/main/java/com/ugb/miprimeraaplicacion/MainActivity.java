6dc33f8243d298b68cd081f6af8f0fcdac56c2af
Mrjaime08 <jaimesegovia2005@gmail.com>
2025-02-02 17:12:50 -0600

    Calculadora de SUMA RESTA MULTIPLICACION Y DIVISION

--- app/src/main/java/com/ugb/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/ugb/miprimeraaplicacion/MainActivity.java
@@ -3,33 +3,67 @@ package com.ugb.miprimeraaplicacion;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     Button btn;
     TextView tempVal;
+    RadioGroup rgb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Referencias a los elementos
         btn = findViewById(R.id.btnCalcular);
+        rgb = findViewById(R.id.rgbOpciones);
+        tempVal = findViewById(R.id.lblRespuesta);
+
+        // Evento del botón
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tempVal = findViewById(R.id.txtNum1);
-                double num1 = Double.parseDouble(tempVal.getText().toString());
-                tempVal = findViewById(R.id.txtNum2);
-                double num2 = Double.parseDouble(tempVal.getText().toString());
-                double respuesta = num1 + num2;
-                tempVal = findViewById(R.id.lblRespuesta);
-                tempVal.setText("Respuesta: "+ respuesta);
+                calcularOperacion();
             }
         });
     }
+
+    private void calcularOperacion() {
+        EditText txtNum1 = findViewById(R.id.txtNum1);
+        EditText txtNum2 = findViewById(R.id.txtNum2);
+        TextView lblRespuesta = findViewById(R.id.lblRespuesta);
+
+        try {
+            double num1 = Double.parseDouble(txtNum1.getText().toString());
+            double num2 = Double.parseDouble(txtNum2.getText().toString());
+            double respuesta = 0;
+
+            int selectedId = rgb.getCheckedRadioButtonId();
+            if (selectedId == R.id.optSuma) {
+                respuesta = num1 + num2;
+            } else if (selectedId == R.id.optResta) {
+                respuesta = num1 - num2;
+            } else if (selectedId == R.id.optMultiplicacion) {
+                respuesta = num1 * num2;
+            } else if (selectedId == R.id.optDivision) {
+                if (num2 != 0) {
+                    respuesta = num1 / num2;
+                } else {
+                    lblRespuesta.setText("Error: División por 0");
+                    return;
+                }
+            }
+
+            lblRespuesta.setText("Respuesta: " + respuesta);
+        } catch (NumberFormatException e) {
+            lblRespuesta.setText("Error: Ingrese números válidos");
+        }
+    }
 }
+
+
